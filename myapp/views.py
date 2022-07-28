620298b7be2719c4222f2432e92819c1ca75c9d7
Nimrakhanzai <nimrakhanzai@yahoo.com>
2022-07-28 01:51:06 -0700

    initial commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse
+import psutil
+import platform
 from datetime import datetime
-x=datetime.now()
 
 # Create your views here.
 def index(request):
     return HttpResponse("This is my app")
 
 def date_time(request):
-    return HttpResponse({"Today's date and time is" : x})
+    x = datetime.datetime.now()
+    html1="<html><body><b> current date and time is: %s </b></body></html>" % x 
+    return HttpResponse(html1)
 
 def sys_info(request):
-    return HttpResponse("This is System Information page")
+    uname = platform.uname()
+    return HttpResponse(f"System: {uname.system}",(f"Node Name: {uname.node}"))
+    return HttpResponse(f"Node Name: {uname.node}")
+    return HttpResponse(f"Release: {uname.release}")
+    return HttpResponse(f"Version: {uname.version}")
+    return HttpResponse(f"Machine: {uname.machine}")
+    return HttpResponse(f"Processor: {uname.processor}")
 
 def calendar(request):
     return HttpResponse("This is calendar page")
